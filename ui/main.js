a81ad90246fcd445dc9a7c69f73a4ae012345375
tmjamal <30651663+tmjamal@users.noreply.github.com>
2017-08-10 13:53:59 +0300

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -37,7 +37,7 @@ submit.onclick=function(){
     for(var i=0;i<names.length;i++){
         list+='<li>'+names[i]+'</li>';
     }
-    var ul=document.getEelementById('namelist');
+    var ul=document.getElementById('namelist');
     ul.innerHTML=list;
 };
 
