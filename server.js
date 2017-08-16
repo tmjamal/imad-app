19499b0173c9d174d5a3afb9c609845b908fd3bc
tmjamal <30651663+tmjamal@users.noreply.github.com>
2017-08-16 09:07:31 +0300

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,9 +97,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 var pool=new Pool(config);
-app.get('/test', function(req, res){
+app.get('/test-db', function(req, res){
     // make a select request
     //return a response with the results
+    pool.query('SELECT * FROM test', function(err, result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+            res.send(JSON.stringify(result));
+        }
+    });
 });
 var counter=0;
 app.get('/counter', function(req, res){
