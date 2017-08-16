2232b9e2d7f485f1618cfbbfd7271729f715d4fe
tmjamal <30651663+tmjamal@users.noreply.github.com>
2017-08-16 09:59:32 +0300

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -109,6 +109,7 @@ app.get('/test-db', function(req, res){
             res.status(500).send(err.toString());
         }
         else{
+            alert('Hi');
             res.send(JSON.stringify(result.rows));
         }
     });
