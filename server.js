26f4711976a19c7bb2d67b508ca1e2e5ae257c15
tmjamal <30651663+tmjamal@users.noreply.github.com>
2017-08-16 10:29:37 +0300

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -130,12 +130,25 @@ app.get('/sumbmit-name',function(req,res){//submit-name?name=xxx
     //JSON:Javascript Object Notation
     res.send(JSON.stringify(names));
 });
-app.get('/:articleName', function(req, res){
+app.get('/articles/:articleName', function(req, res){
     //articleName==article-one
     //articles[articleName]=={}content object for article one
-    var articleName = req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
+    
+    //SELECT * FROM article WHERE title=article-one
+    pool.query("SELECT * FROM article WHERE title = '"+ req.params.articleName +"'", fuction(err, result){
+        if(err){
+            res.status(500).send(err.toString());
+            }else{
+                if(result.rows.length===0){
+                    res.status(404).send('Article not found');
+                }else{
+                    var articleData=result.rows[0];
+                    res.send(createTemplate(articleData));
+                }
+            }
+    });
 });
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
