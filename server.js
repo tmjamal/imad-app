37303d4f0d88a4f2f5a445a6d3dcecf9e556fb00
tmjamal <30651663+tmjamal@users.noreply.github.com>
2017-08-16 09:01:58 +0300

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,8 +96,8 @@ function createTemplate(data){
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
-app.get('/', function(req, res){
+var pool=new Pool(config);
+app.get('/test', function(req, res){
     // make a select request
     //return a response with the results
 });
