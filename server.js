86f1b278767ca76c6762d13109d5357bc78e3d5c
tmjamal <30651663+tmjamal@users.noreply.github.com>
2017-08-16 08:53:16 +0300

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,6 +90,11 @@ function createTemplate(data){
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+app.get('/', function(req, res){
+    // make a select request
+    //return a response with the results
+})
 var counter=0;
 app.get('/counter', function(req, res){
     counter=counter+1;
